d9549f0bd8425f17568b42a5bcbaed1eba7857e9
liubo <1353648067@qq.com>
2022-03-23 15:46:32 +0800

    挑战5 自定义减法函数

---
+++ subtra.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int subtra(int a, int b);
+
+int main()
+{
+    cout << subtra(9, 5) << endl;
+    return 0;
+}
+
+int subtra(int a, int b)
+{
+    return a - b;
+}
