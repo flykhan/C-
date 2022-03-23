4243cdc95de289e101b5c2cda2a3711507672cc9
liubo <1353648067@qq.com>
2022-03-23 15:15:29 +0800

    参数传递 引用传递

---
+++ swapxy1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+    int t = a;
+    a = b;
+    b = t;
+}
+
+int main()
+{
+    int x = 3;
+    int y = 4;
+    cout<<"x="<<x<<", y="<<y<<endl;
+    swap(x,y);
+    cout<<"x="<<x<<", y="<<y<<endl;
+    return 0;
+}
