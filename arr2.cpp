98556bd3b562be2d3f8d87be92ed3c1b82acb2f5
liubo <1353648067@qq.com>
2022-03-25 15:27:10 +0800

    数组访问2

---
+++ arr2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[3][5] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}; //初始化数组
+    cout << a[1][2] << endl;                                           //访问数组
+    return 0;
+}
