2af035d9a68d3c5b7c914e6e8b1603748872ed4d
liubo <1353648067@qq.com>
2022-03-25 14:17:28 +0800

    类的定义 private

--- pritest.cpp
+++ pritest.cpp
@@ -33,3 +33,5 @@ int main()
     cout<<"width:"<<wid1.getwidth()<<endl;
     return 0;
 }
+
+//此程序可以分为相对独立的两个部分，一是 area 类及其公有成员函数的定义，二是主函数 main()。私有成员 width 只能先被类中成员函数访问，再通过相关的公共成员函数实现外部访问。若在类外部使用 area.width=3.2; 方式访问 area 类的私有成员 width，则会报错。
