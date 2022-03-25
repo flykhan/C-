e0d8e5978b6e2cf86dccdd00f2a653444581c1de
liubo <1353648067@qq.com>
2022-03-25 12:23:03 +0800

    类的定义 protected

---
+++ protest.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class area
+{
+protected: //定义受保护的成员
+    double width;
+};
+
+class squarearea : area //定义squarearea子类
+{
+public:                              //定义公有成员
+    void setsquarewidth(double wid); //定义成员函数
+    double getsquarewidth(void);
+};
+
+//定义子类的成员函数
+void squarearea::setsquarewidth(double wid)
+{
+    width = wid; //通过squarearea子类访问area类中受保护的成员width
+}
+
+double squarearea::getsquarewidth(void)
+{
+    return width;
+}
+
+//程序的主函数
+int main()
+{
+    squarearea square; //定义对象
+    square.setsquarewidth(3.1);//设置宽度为3.1
+    cout<<"width:"<<square.getsquarewidth()<<endl;//输出设置的宽度
+    return 0;
+}
